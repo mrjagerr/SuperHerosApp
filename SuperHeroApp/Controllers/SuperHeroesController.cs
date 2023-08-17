b9933969a4ca663e398933a307fdf1bf6e242b4c
skyeler yearsley <s2krighty@gmail.com>
2023-08-17 14:42:18 -0500

    Addition of New SuperHeroes controller and Linq

---
+++ SuperHeroApp/Controllers/SuperHeroesController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroApp.Models;
+
+namespace SuperHeroApp.Controllers
+{
+    public class SuperHeroesController : Controller
+    {
+        // GET: SuperHeroesController
+        public ActionResult Index()
+        {
+
+            //Linq query to retrieve all the rows from the table
+            var superheroes = "";
+            return View();
+        }
+
+        // GET: SuperHeroesController/Details/5
+        public ActionResult Details(int id)
+        {
+            //Linq query to find specific row from table
+            return View();
+        }
+
+        // GET: SuperHeroesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SuperHeroesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(SuperHero superHero)
+        {
+            try
+            {
+                //Linq Add superhero to the list
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperHeroesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperHeroesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperHeroesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperHeroesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- SuperHeroApp/Data/ApplicationDbContext.cs
+++ SuperHeroApp/Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-
+using SuperHeroApp.Models;
 namespace SuperHeroApp.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
