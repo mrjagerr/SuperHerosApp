11ec2fa9bdc4163e4dec0847ee4873b41f9ecbf6
skyeler yearsley <s2krighty@gmail.com>
2023-08-17 14:02:18 -0500

    Construction of SuperHero class

--- SuperHeroApp/Models/SuperHero.cs
+++ SuperHeroApp/Models/SuperHero.cs
@@ -1,6 +1,16 @@
-﻿namespace SuperHeroApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperHeroApp.Models
 {
     public class SuperHero
     {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryPower { get; set; }
+        public string SecondaryPower { get; set;}
+        public string Catchphrase{ get; set; }
+
     }
 }
